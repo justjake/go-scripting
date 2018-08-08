13b3ff46e7e5ef7235de7c589ed2847b7b8510b1
Jake Teton-Landis <jake.teton-landis@airbnb.com>
2018-08-07 19:11:37 -0700

    WIP

--- bin/smoketest.go
+++ bin/smoketest.go
@@ -1,5 +1,7 @@
 package main
 
+// go:generate go run ./generate_script_ui.go -out ui.go
+
 import (
 	"fmt"
 	"github.com/justjake/go-scripting/env"
@@ -7,21 +9,59 @@ import (
 	"os"
 )
 
-var Vars = env.NewVars()
-var Args = env.SystemArgs()
-var Sh = &shell.Shell{
-	Stdout: os.Stdout,
-	Stderr: os.Stderr,
+// UI provides our main and such
+var UI = &script.UI{
+	Name:  "smoketest",
+	Short: "validates ideas in go scripting",
+	Long: `Smoketest uses different featuers from the go-scripting packages. It
+serves both as an example and as a unit test.`,
 }
 
-var FIRST = Vars.DefaultGetter("FIRST", "Jake")
-var LAST = Vars.DefaultGetter("LAST", "Teton-Landis")
-var NAME = Vars.MemoGetter("NAME", func() string {
-	return FIRST() + " " + LAST()
-})
+type script struct {
+	env.Args
+	env.Vars
+	shell.Interface
+}
+
+func newScript() *script {
+	return &script{
+		Args: env.SystemArgs(),
+		Vars: *env.NewVars(),
+		Interface: &shell.Shell{
+			Stdout: os.Stdout,
+			Stderr: os.Stderr,
+		},
+	}
+}
+
+// FIRST is the user's first name
+func (sh *script) FIRST() string {
+	return sh.Get("FIRST", "Jake")
+}
+
+// LAST is the user's last name
+func (sh *script) LAST() string {
+	return sh.Get("LAST", "Teton-Landis")
+}
+
+// NAME is the user's full name, computed from FIRST and LAST.
+func (sh *script) NAME() string {
+	return sh.GetMemo("NAME", func() string { return sh.FIRST() + " " + sh.LAST() })
+}
+
+func (sh *script) somePrivateAccessor() string {
+	return "blarg"
+}
+
+// Greet shows a greeting to the user.
+// Optional: NAME
+func (sh *script) Greet() string {
+	sh.Runf("echo Hello dearest %s: %s", sh.NAME(), sh.somePrivateAccessor())
+}
 
 func main() {
+	sh := newScript()
 	fmt.Println(shell.Escape("; exit 1"))
 	fmt.Println(shell.ScriptPrintf("%s", "; exit 1"))
-	Sh.Runf("echo first: %s, last: %s, name: %s", FIRST(), LAST(), NAME())
+	sh.Runf("echo first: %s, last: %s, name: %s", sh.FIRST(), sh.LAST(), sh.NAME())
 }
